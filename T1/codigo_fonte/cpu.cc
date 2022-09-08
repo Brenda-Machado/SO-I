d608e1b2c8e5eafd9678efb2ebbc2da66bb49a97
Brenda-Machado <araucaria2011@gmail.com>
2022-09-08 17:39:25 -0300

    metodos ucontext - falta a stack

--- T1/codigo_fonte/cpu.cc
+++ T1/codigo_fonte/cpu.cc
@@ -7,25 +7,28 @@ __BEGIN_API
 void CPU::Context::save()
 {
     //adicionar implementação
-    //salvar na pilha - setContext
+    //salvar na pilha - alocar
+    //int setcontext(const ucontext_t *);
 }
 
 void CPU::Context::load()
 {
     //adicionar implementação
-    //carregar da pilha - getContext
+    //carregar da pilha - desalocar
+    //int getcontext(ucontext_t *);
 }
 
 CPU::Context::~Context()
 {
     //adicionar implementação
-    //cria contexto - makeContext
+    //destroi contexto - libera a memoria alocada na pilha
 }
 
 void CPU::switch_context(Context *from, Context *to)
 {
      //implementação do método
      //troca contextos - swapContext
+     //int swapcontext(Context *from, Context *to);
 }
 
 __END_API
