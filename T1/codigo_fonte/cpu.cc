01b5fc9e9196bbbd0c69c954ac4d955ea43750b0
Brenda-Machado <araucaria2011@gmail.com>
2022-09-10 20:39:29 -0300

    metodos p2

--- T1/codigo_fonte/cpu.cc
+++ T1/codigo_fonte/cpu.cc
@@ -1,34 +1,27 @@
 #include "cpu.h"
 #include <iostream>
-#include "ucontext.h" //arquivo com os metodos context
+#include <ucontext.h>
 
 __BEGIN_API
 
 void CPU::Context::save()
 {
-    //adicionar implementação
-    //salvar na pilha - alocar
-    //int setcontext(const ucontext_t *);
+    getcontext(&_context);
 }
 
 void CPU::Context::load()
 {
-    //adicionar implementação
-    //carregar da pilha - desalocar
-    //int getcontext(ucontext_t *);
+    setcontext(&_context);
 }
 
 CPU::Context::~Context()
 {
-    //adicionar implementação
-    //destroi contexto - libera a memoria alocada na pilha
+    delete[] _stack;
 }
 
 void CPU::switch_context(Context *from, Context *to)
 {
-     //implementação do método
-     //troca contextos - swapContext
-     //int swapcontext(Context *from, Context *to);
+    swapcontext(&(from->_context), &(to->_context));
 }
 
 __END_API
