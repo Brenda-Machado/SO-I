5dd4dfd44575cbe2bfd7d2daf786b10f5634d104
Brenda-Machado <araucaria2011@gmail.com>
2022-09-10 20:38:24 -0300

    metodos

---
+++ T1/codigo_fonte/cpu.cpp
@@ -0,0 +1,27 @@
+#include "cpu.h"
+#include <iostream>
+#include <ucontext.h>
+
+__BEGIN_API
+
+void CPU::Context::save()
+{
+    getcontext(&_context);
+}
+
+void CPU::Context::load()
+{
+    setcontext(&_context);
+}
+
+CPU::Context::~Context()
+{
+    delete[] _stack;
+}
+
+void CPU::switch_context(Context *from, Context *to)
+{
+    swapcontext(&(from->_context), &(to->_context));
+}
+
+__END_API
