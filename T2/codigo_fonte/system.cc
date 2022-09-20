5486357640289b229a4dbe9f5c8427e019479fc8
Brenda-Machado <araucaria2011@gmail.com>
2022-09-20 12:03:14 -0300

    debbug do system funciona

--- T2/codigo_fonte/system.cc
+++ T2/codigo_fonte/system.cc
@@ -1,10 +1,12 @@
 #include "system.h"
 #include <iostream>
+#include "debug.h"
 
 __BEGIN_API
 
 void System::init(){
-	setvbuf (stdout, 0, _IONBF, 0) ;
+	db<System>(TRC) << "System::init() chamado\n";
+	setvbuf (stdout, 0, _IONBF, 0);
 }
 
 __END_API
